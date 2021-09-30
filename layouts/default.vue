6095f2d02d287c1d96606b39b994de2eed9eb16f
Abdul Mohaimen <mohaimen707@gmail.com>
2021-09-30 22:17:44 +0600

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-toolbar color="indigo dark">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Weather App</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn>
+            <n-link to="/">Home</n-link>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+a {
+  color: #ffffff !important;
+  text-decoration: none;
+}
+</style>
